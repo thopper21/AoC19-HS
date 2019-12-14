3fbd20f30edd5251f0ed4dd62a2f8e06cb8a6a2b
Taylor Hopper <taylorhopper@outlook.com>
2019-12-14 14:22:34 -0800

    Solve Day 1B

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,15 @@ module Main where
 import           Day1
 
 run file fn = do
-    input <- readFile file
-    let result = fn input
-    print result
+  input <- readFile file
+  let result = fn input
+  print result
 
-day1 = run "input/Day1.txt" Day1.solveA
+day1 = run "input/Day1.txt"
+
+day1A = day1 Day1.solveA
+
+day1B = day1 Day1.solveB
 
 main :: IO ()
-main = day1
+main = day1B
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,10 +1,22 @@
 module Day1
   ( solveA
+  , solveB
   ) where
 
 import           Data.Monoid
 
-fuelRequirement :: Integer -> Integer
 fuelRequirement mass = (mass `div` 3) - 2
 
-solveA input = getSum $ mconcat $ Sum . fuelRequirement . read <$> lines input
+fullFuelRequirement mass =
+  let fuelMass = fuelRequirement mass
+      remainder =
+        if fuelMass > 0
+          then max (fullFuelRequirement fuelMass) 0
+          else 0
+   in fuelMass + remainder
+
+solve fn input = getSum $ mconcat $ Sum . fn . read <$> lines input
+
+solveA = solve fuelRequirement
+
+solveB = solve fullFuelRequirement
