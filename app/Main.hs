0b1317adf7150786cf9e60ca1eeb5c1b2657104b
Taylor Hopper <taylorhopper@outlook.com>
2019-12-14 16:55:58 -0800

    Solve Day 2 Part 1

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import           Runner
 
 main :: IO ()
-main = run Day1 Part2
+main = run Day2 Part1
--- src/Runner.hs
+++ src/Runner.hs
@@ -1,18 +1,22 @@
 module Runner where
 
-import qualified Day1Solution
+import qualified Day1Solution as D1
+import qualified Day2Solution as D2
 
-data Day =
-  Day1
+data Day
+  = Day1
+  | Day2
 
 data Part
   = Part1
   | Part2
 
 toDayNumber Day1 = 1
+toDayNumber Day2 = 2
 
-solution Day1 Part1 = Day1Solution.solveA
-solution Day1 Part2 = Day1Solution.solveB
+solution Day1 Part1 = D1.solveA
+solution Day1 Part2 = D1.solveB
+solution Day2 Part1 = D2.solveA
 
 input day = "input/Day" ++ show (toDayNumber day) ++ ".txt"
 
