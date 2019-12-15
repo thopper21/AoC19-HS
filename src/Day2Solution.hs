95ae1ac28b4d33781c98f095196100b1aac1176c
Taylor Hopper <taylorhopper@outlook.com>
2019-12-14 17:43:26 -0800

    Clean up Day 2

--- src/Day2Solution.hs
+++ src/Day2Solution.hs
@@ -3,42 +3,40 @@ module Day2Solution
   , solveB
   ) where
 
-import           Data.IntMap.Lazy
-import           Data.List        (find)
-import           Data.Maybe
+import           Data.IntMap.Lazy (IntMap, empty, insert, (!))
+import           Data.List        (find, foldl)
+import           Data.Maybe       (fromJust)
 
 toProgram :: Int -> Int -> [Int] -> IntMap Int
-toProgram noun verb xs =
-  let toProgram' [] _     = empty
-      toProgram' (x:xs) i = insert i x $ toProgram' xs (i + 1)
-   in insert 2 verb $ insert 1 noun $ toProgram' xs 0
+toProgram noun verb =
+  let fromList xs =
+        foldl (\program (i, x) -> insert i x program) empty (zip [0 ..] xs)
+   in insert 2 verb . insert 1 noun . fromList
 
-commaToSpace ',' = ' '
-commaToSpace c   = c
-
-replaceAt [] _ _           = []
-replaceAt (x:xs) 0 value   = value : xs
-replaceAt (x:xs) pos value = x : replaceAt xs (pos - 1) value
-
-op leftPos rightPos outPos program fn =
-  let left = program ! leftPos
-      right = program ! rightPos
-      result = fn left right
-   in insert outPos result program
+replace value with =
+  let replacement c =
+        if c == value
+          then with
+          else c
+   in replacement
 
 run =
   let run' position program =
         let opCode = program ! position
             arg offset = program ! (position + offset)
             next = run' (position + 4)
-            op' = op (arg 1) (arg 2) (arg 3)
+            binOp fn program =
+              let left = program ! arg 1
+                  right = program ! arg 2
+                  result = fn left right
+               in insert (arg 3) result program
          in case opCode of
-              1  -> next $ op' program (+)
-              2  -> next $ op' program (*)
+              1  -> next $ binOp (+) program
+              2  -> next $ binOp (*) program
               99 -> toInteger $ program ! 0
    in run' 0
 
-parseInput input = read <$> words (commaToSpace <$> input)
+parseInput input = read <$> words (replace ',' ' ' <$> input)
 
 solveA input = run $ toProgram 12 2 $ parseInput input
 
