5865983dff465a573e518f9e4650ca5fc74e5cf9
Taylor Hopper <taylorhopper@outlook.com>
2019-12-27 08:49:16 -0800

    Tidy up IntCode interface

--- src/Day2Solution.hs
+++ src/Day2Solution.hs
@@ -3,18 +3,22 @@ module Day2Solution
   , solveB
   ) where
 
-import           Data.List        (find, foldl)
-import           Data.List.Split  (splitOn)
-import           Data.Maybe       (fromJust)
+import           Data.List       (find)
+import           Data.List.Split (splitOn)
+import           Data.Maybe      (fromJust)
 import           IntCode
 
 parseInput = fmap read . splitOn ","
 
-solveA input = run $ toProgram 12 2 $ parseInput input
+toProgram noun verb = set 2 verb . set 1 noun . program
+
+result = toInteger . get 0 . run
+
+solveA input = result $ toProgram 12 2 $ parseInput input
 
 solveB input =
   let parsed = parseInput input
-      pred (noun, verb) = run (toProgram noun verb parsed) == 19690720
+      pred (noun, verb) = result (toProgram noun verb parsed) == 19690720
       (noun, verb) =
         fromJust $
         find pred [(noun, verb) | noun <- [0 .. 99], verb <- [0 .. 99]]
--- src/IntCode.hs
+++ src/IntCode.hs
@@ -1,14 +1,17 @@
 module IntCode
-  ( toProgram
+  ( program
   , run
+  , set
+  , get
   ) where
 
 import           Data.IntMap.Lazy (IntMap, fromDistinctAscList, insert, (!))
 
-toProgram noun verb =
-  let fromList xs = fromDistinctAscList $ zip [0 ..] xs
-      setInput noun verb = insert 2 verb . insert 1 noun
-   in setInput noun verb . fromList
+program = fromDistinctAscList . zip [0 ..]
+
+set = insert
+
+get = flip (!)
 
 run =
   let run' position program =
@@ -23,5 +26,5 @@ run =
          in case opCode of
               1  -> next $ binOp (+) program
               2  -> next $ binOp (*) program
-              99 -> toInteger $ program ! 0
+              99 -> program
    in run' 0
