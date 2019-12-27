390d6749e92d0da43e76672ebf5afb59427dcb98
Taylor Hopper <taylorhopper@outlook.com>
2019-12-26 21:29:38 -0800

    Solve Day 4 Part 1

--- src/Day4Solution.hs
+++ src/Day4Solution.hs
@@ -2,4 +2,20 @@ module Day4Solution
   ( solveA
   ) where
 
-solveA _ = 42
+import           Data.List.HT
+
+digits x = reverse $ digits' x
+  where
+    digits' 0 = []
+    digits' x = (x `mod` 10) : digits' (x `div` 10)
+
+adjacentDigits = or . mapAdjacent (==)
+
+increasingDigits = and . mapAdjacent (<=)
+
+matches x = adjacentDigits x && increasingDigits x
+
+solveA _ =
+  let begin = 130254
+      end = 678275
+   in toInteger . length . filter matches . fmap digits $ [begin .. end]
