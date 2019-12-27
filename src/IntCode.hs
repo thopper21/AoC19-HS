972362abec51cccaf26bd192a4bf0897f4551556
Taylor Hopper <taylorhopper@outlook.com>
2019-12-27 10:22:32 -0800

    Clean up IntCode

--- src/IntCode.hs
+++ src/IntCode.hs
@@ -24,20 +24,20 @@ emptyProgram = Program {_memory = empty, _ip = 0, _input = [], _output = []}
 
 program input = set memory (fromDistinctAscList $ zip [0 ..] input) emptyProgram
 
-setMemory pos val = over memory $ insert pos val
+setMemory pos = over memory . insert pos
 
 getMemory pos program = view memory program ! pos
 
 run program =
   let opCode = getMemory (view ip program) program
       arg offset = getMemory (view ip program + offset) program
-      binOp fn program =
+      binOp fn =
         let left = getMemory (arg 1) program
             right = getMemory (arg 2) program
             result = fn left right
          in setMemory (arg 3) result program
       next program = run $ over ip (+ 4) program
    in case opCode of
-        1  -> next $ binOp (+) program
-        2  -> next $ binOp (*) program
+        1  -> next $ binOp (+)
+        2  -> next $ binOp (*)
         99 -> program
