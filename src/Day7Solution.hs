39c7884ee65bb5c1374ab5ec94f15336b062a41e
Taylor Hopper <taylorhopper@outlook.com>
2019-12-28 21:13:08 -0800

    Solve Day 7 Part 1

--- src/Day7Solution.hs
+++ src/Day7Solution.hs
@@ -2,4 +2,15 @@ module Day7Solution
   ( solveA
   ) where
 
-solveA _ = 42
+import           Data.List
+import           IntCode
+
+runThrusters program = foldl runThrusterPhase 0
+  where
+    runThrusterPhase prevOut currIn =
+      diagnosticCode . run $ setInput [currIn, prevOut] program
+
+maxThruster program =
+  maximum $ fmap (runThrusters program) (permutations [0 .. 4])
+
+solveA = toInteger . maxThruster . parseProgram
