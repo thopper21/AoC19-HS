3a9e511b8d5e48afc1fe9cc674de48849ab5590a
Taylor Hopper <taylorhopper@outlook.com>
2019-12-30 19:14:33 -0800

    Combine solutions to Day 7

--- src/Day7Solution.hs
+++ src/Day7Solution.hs
@@ -6,32 +6,24 @@ module Day7Solution
 import           Data.List
 import           IntCode
 
-runThrusters program = foldl runThrusterPhase 0
-  where
-    runThrusterPhase prevOut currIn =
-      let (AwaitingInput resume, _) = run program
-          (AwaitingInput resume', _) = resume currIn
-          (Terminated, program') = resume' prevOut
-       in lastOutput program'
-
-maxThruster program = maximum $ runThrusters program <$> permutations [0 .. 4]
-
-solveA = toInteger . maxThruster . parseProgram
-
 runFeedbackThrusters input Terminated _ _ _ _ = input
 runFeedbackThrusters input (AwaitingInput resume) stateB stateC stateD stateE =
   let (stateA, program) = resume input
-      loopOutput = lastOutput program
-   in runFeedbackThrusters loopOutput stateB stateC stateD stateE stateA
+      input' = lastOutput program
+   in runFeedbackThrusters input' stateB stateC stateD stateE stateA
 
 startThruster program setting =
   let (AwaitingInput resume, _) = run program
    in resume setting
 
-startThrusters program settings =
+runThrusters program settings =
   let [a, b, c, d, e] = fst . startThruster program <$> settings
    in runFeedbackThrusters 0 a b c d e
 
-solveB input =
-  let program = parseProgram input
-   in toInteger . maximum $ startThrusters program <$> permutations [5 .. 9]
+maxSignal settings program = maximum $ runThrusters program <$> permutations settings
+
+solve settings = toInteger . maxSignal settings . parseProgram
+
+solveA = solve [0 .. 4]
+
+solveB = solve [5 .. 9]
