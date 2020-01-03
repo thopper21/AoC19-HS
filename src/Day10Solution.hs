dd4fe1e17060d82943c3c99deedaf6e834c83846
Taylor Hopper <taylorhopper@outlook.com>
2020-01-03 08:09:20 -0800

    Solve Day 10 Part 1

--- src/Day10Solution.hs
+++ src/Day10Solution.hs
@@ -1,8 +1,30 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day10Solution
   ( solveA
   , solveB
   ) where
 
-solveA _ = 42
+import           Data.List
+
+parseInput = concat . zipWith coordinates [0 ..] . lines
+  where
+    coordinates row line =
+      (, row) . fst <$> filter ((== '#') . snd) (zip [0 ..] line)
+
+direction (x, y) (x', y') =
+  let dx = x' - x
+      dy = y' - y
+      divisor = gcd dx dy
+   in (dx `div` divisor, dy `div` divisor)
+
+countUnique = length . group . sort
+
+uniqueDirections positions position =
+  countUnique $ direction position <$> filter (/= position) positions
+
+solveA input =
+  let positions = parseInput input
+   in maximum $ uniqueDirections positions <$> positions
 
 solveB _ = 42
